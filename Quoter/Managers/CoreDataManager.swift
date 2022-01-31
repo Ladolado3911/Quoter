d085aa2c4679e15dfcdd331598cf913359a514a2
Lado <66564697+Ladolado3911@users.noreply.github.com>
2022-01-31 15:35:02 +0400

    - adding get author function

--- Quoter/Managers/CoreDataManager.swift
+++ Quoter/Managers/CoreDataManager.swift
@@ -76,31 +76,75 @@ class CoreDataManager {
         }
     }
     
+    static func getAuthor(authorName: String) -> NSManagedObject? {
+        guard let context = context else { return nil }
+        
+        let requestAuthors = NSFetchRequest<AuthorCore>(entityName: "AuthorCore")
+        
+        do {
+            let authors = try context.fetch(requestAuthors)
+            for authorCore in authors {
+                if authorCore.name == authorName {
+                    return authorCore
+                }
+            }
+            return nil
+        }
+        catch {
+            print(error)
+            return nil
+        }
+    }
+    
     static func addQuote(quoteVM: QuoteVM, authorImageData: Data) {
         guard let context = context else { return }
 
         let quote = QuoteCore(context: context)
         
-        if CoreDataManager.isAuthorInCoreData(authorName: quoteVM.author) {
-            quote.content = quoteVM.content
+        let isAuthorInCore: Bool = CoreDataManager.isAuthorInCoreData(authorName: quoteVM.author)
+        let isQuoteInCore: Bool = CoreDataManager.isQuoteInCoreData(quoteVM: quoteVM)
+        
+        if isAuthorInCore && isQuoteInCore {
+//            quote.content = quoteVM.content
+            print("CAN NOT ADD QUOTE, IT ALREADY EXISTS IN CORE DATA")
+        }
+        else if isAuthorInCore && !isQuoteInCore {
+            print("THERE IS AUTHOR IN CORE BUT NOT THIS QUOTE, ADD THIS QUOTE AS NEW QUOTE")
+            // get author and add this quote to that author
+            if let authorObject = CoreDataManager.getAuthor(authorName: quoteVM.author) {
+                quote.content = quoteVM.content
+                authorObject.q
+                do {
+                    try context.save()
+                }
+                catch {
+                    print(error)
+                }
+            }
+        }
+        else if !isAuthorInCore && isQuoteInCore {
+            print("AUTHOR IS NOT IN CORE AND QUOTE IS IN CORE. DELETE RUBBISH QUOTE FROM CORE")
         }
         else {
+            print("THERE IS NOTHING LIKE THIS IN CORE. NO AUTHOR AND NO QUOTE. ADD BOTH")
             let author = AuthorCore(context: context)
             author.name = quoteVM.author
             author.image = authorImageData
         }
+        
+
         
 //        model2.firstName = model.givenName
 //        model2.lastName = model.familyName
 //        model2.mobile = model.phoneNumbers.first?.value.stringValue
         
 
-        do {
-            try context.save()
-        }
-        catch {
-            print(error)
-        }
+//        do {
+//            try context.save()
+//        }
+//        catch {
+//            print(error)
+//        }
     }
     
     
