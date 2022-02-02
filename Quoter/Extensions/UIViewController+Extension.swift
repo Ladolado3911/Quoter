5884a06381ef3b349d77e2c5e22f11aeaa44c2c5
Lado <66564697+Ladolado3911@users.noreply.github.com>
2022-02-02 16:47:14 +0400

    - adding new modal alert pop up function on every view controller

---
+++ Quoter/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Extension.swift
+//  Quoter
+//
+//  Created by Lado Tsivtsivadze on 2/2/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentModalAlert(text: String) {
+        
+    }
+}
---
+++ Quoter/Presentation/SharedElements/Views/ModalAlertView.swift
@@ -0,0 +1,25 @@
+//
+//  ModalAlertView.swift
+//  Quoter
+//
+//  Created by Lado Tsivtsivadze on 2/2/22.
+//
+
+import UIKit
+import SnapKit
+
+class ModalAlertView: UIView {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    private func buildSubviews() {
+        
+    }
+    
+    private func buildConstraints() {
+        
+    }
+}
