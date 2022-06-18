ca43aa588c5d486a36f51eefb3ba9839418f6228
Dimitri Altunashvili <66564697+Ladolado3911@users.noreply.github.com>
2022-06-18 20:10:49 +0400

    started building header view

--- Quoter/Presentation/Scenes/Author/Subviews/AuthorTableHeaderView.swift
+++ Quoter/Presentation/Scenes/Author/Subviews/AuthorTableHeaderView.swift
@@ -9,18 +9,43 @@ import UIKit
 
 class AuthorTableHeaderView: UIView {
     
+    let imageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.contentMode = .scaleAspectFill
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        return imgView
+    }()
+    
+    let aboutLabel: UILabel = {
+        let aboutLabel = UILabel()
+        aboutLabel.text = "About"
+        aboutLabel.translatesAutoresizingMaskIntoConstraints = false
+        return aboutLabel
+    }()
+    
+    let descLabel: UILabel = {
+        let descLabel = UILabel()
+        descLabel.numberOfLines = 3
+        descLabel.translatesAutoresizingMaskIntoConstraints = false
+        return descLabel
+    }()
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        backgroundColor = .green
+        backgroundColor = .clear
         buildSubviews()
         buildConstraints()
     }
     
     private func buildSubviews() {
-        
+        addSubview(imageView)
+        addSubview(aboutLabel)
+        addSubview(descLabel)
     }
     
     private func buildConstraints() {
-        
+        NSLayoutConstraint.activate([
+            
+        ])
     }
 }
